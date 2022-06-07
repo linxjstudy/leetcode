2e4558a2e921796e093a0b370c0f8631628eb086
linxjstudy <linxjstudy@126.com>
2022-06-08 01:45:53 +0800

    No.1 Two-Sum

---
+++ 1.Two-Sum.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        return res;
+    }
+};
+int main() {
+    int a = 0;
+    cout << a;
+    return 0;
+}
+// @lc code=end
+
